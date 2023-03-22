81106e7892bcce9a51fcdc1f7b4617d9a205658d
Rodrigo Ramos <digojramos@gmail.com>
2023-03-22 17:59:18 -0300

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header";
+
 export default function App() {
 
   return (
-    <h1>Olá mundo!!</h1>
+    <Header />
   )
 }
 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+export default function Header() {
+    return (
+        <header className="scg-container-header">
+            <div className="scg-container">
+                <h1>SISTEMA DE CONTROLE DE GASTOS</h1>
+            </div>
+        </header>
+    )
+}
